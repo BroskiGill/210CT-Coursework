9187854237d65db1bcc538f57047c39d68d31a0b
BroskiGill <onkar@hotmail.co.uk>
2016-12-02 13:51:25 +0000

    Update Q6Coursework.py

--- Q6Coursework.py
+++ Q6Coursework.py
@@ -1,3 +1,16 @@
+##Pseudocode
+##  reverseString(s)
+##  countWord<- COUNT s
+
+##  IF countWord=0
+##      RETURN ""
+##  ELSE
+##      RETURN reverseString s[1:] + s[0]+ " "
+##userInput<-INPUT "Please enter words to be reversed: "
+##PRINT reverseString(userInput.SPLIT(" ")
+
+
+
 #This code will let a user input words that will be revered as an output
 def reverseString(s):                                                       #1
     
