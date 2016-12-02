90ec001ec1be131299a71e56948bf64bd7a10464
BroskiGill <onkar@hotmail.co.uk>
2016-12-02 16:53:23 +0000

    Update Q5Coursework.py

--- Q5Coursework.py
+++ Q5Coursework.py
@@ -1,42 +1,50 @@
 #Pseudocode
-##b<-[[1,5,3,2],
-##   [6,8,2,1],
-##   [7,1,3,7]]
-## c<-[[8,2,4,2],
-##    [6,8,2,1]
-##    [7,1,3,7]]
+##b<-[[1,5,3,2],                                                                     #1
+##   [6,8,2,1],                                                                      #1
+##   [7,1,3,7]]                                                                      #1
+##
+## c<-[[8,2,4,2],                                                                    #1
+##    [6,8,2,1]                                                                      #1
+##    [7,1,3,7]]                                                                     #1
 ##
 
-## matrixAdd(b,C)
-## r<-[[0,0,0,0],
-##     [0,0,0,0],
-##     [0,0,0,0]]
+## matrixAdd(b,C)                                                                    #1
+## r<-[[0,0,0,0],                                                                    #1
+##     [0,0,0,0],                                                                    #1
+##     [0,0,0,0]]                                                                    #1
 ##COMMENT  going through rows
-## for i in from COUNT b
+## for i in from COUNT b                                                             #n
 ##COMMENT   going through columns
-##     for j in from COUNT b[0]
-##          r<-r[i][j]=b[i][j]+c[i][j]
-## return r
+##     for j in from COUNT b[0]                                                      #n
+##          r<-r[i][j]=b[i][j]+c[i][j]                                               #n^2
+## return r                                                                          #1
 
-## matrixSub(b,C)
-## r<-[[0,0,0,0],
-##     [0,0,0,0],
-##     [0,0,0,0]]
-## for i in from COUNT b
-##     for j in from COUNT b[0]
-##          r<-r[i][j]=b[i][j]-c[i][j]
-## return r
+## matrixSub(b,c)                                                                    #1
+## r<-[[0,0,0,0],                                                                    #1
+##     [0,0,0,0],                                                                    #1
+##     [0,0,0,0]]                                                                    #1
+##COMMENT  going through rows
+## for i in from COUNT b                                                             #n
+##COMMENT   going through columns
+##     for j in from COUNT b[0]                                                      #n
+##          r<-r[i][j]=b[i][j]-c[i][j]                                               #n^2
+## return r                                                                          #1
 
-## matrixMulti(b,C)
-## r<-[[0,0,0,0],
-##     [0,0,0,0],
-##     [0,0,0,0]]
-## for i in from COUNT b
-##     for j in from COUNT b[0]
-##          r<-r[i][j]=b[i][j]*c[i][j]
-## return r
-##e<-matrixMulti(b,c)
-##f<-matrixAdd(b,c)
-##a<-matrixSub(e)
-## PRINT a
+## matrixMulti(b,c)                                                                  #1
+## r<-[[0,0,0,0],                                                                    #1
+##     [0,0,0,0],                                                                    #1
+##     [0,0,0,0]]                                                                    #1
+##COMMENT  going through rows
+## for i in from COUNT b                                                             #n
+##COMMENT   going through columns
+##     for j in from COUNT b[0]                                                      #n
+##          r<-r[i][j]=b[i][j]*c[i][j]                                               #n^2
+## return r                                                                          #1
+##
+##e<-matrixMulti(b,c)                                                                #1
+##f<-matrixAdd(b,c)                                                                  #1
+##a<-matrixSub(e)                                                                    #1               
+## PRINT a                                                                           #1
+## bigOtotal<- 25+6n+3n^2
+## The big O notation of this is 
 
