84d0a9f7e788b442b6b2cc452a30e775a62dfdc2
BroskiGill <onkar@hotmail.co.uk>
2016-12-02 17:15:40 +0000

    Update Q8Coursework.py

--- Q8Coursework.py
+++ Q8Coursework.py
@@ -1,3 +1,15 @@
+#Pseudocode
+## removeVowels(word)
+## vowels<-["A","E","I","O","U","a","e","i","o","u"]
+## IF word =""
+##      return removeVowels
+## ELSE
+##   FOR i IN word 
+##       IF i IN vowels
+##           word<-word REPLACE("")
+##   RETURN word
+## userInput<-INPUT "Please enter a word for which the vowels will be removed: ")
+## PRINT removeVowels(userInput)
 
 def removeVowels(word):
     vowels=["A","E","I","O","U","a","e","i","o","u"]
