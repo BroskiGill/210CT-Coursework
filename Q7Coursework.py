70413d362ce52556114c61d0a15dc2a53cf8b02e
BroskiGill <onkar@hotmail.co.uk>
2016-12-02 14:06:06 +0000

    Update Q7Coursework.py

--- Q7Coursework.py
+++ Q7Coursework.py
@@ -1,4 +1,17 @@
 import math
+##Pseudocode
+## pn(n)
+## checkPrimeNumber(x,y)
+##      IF y<=1
+##         RETURN n," is a prime number"
+##      ELSE IF x mod y=0
+##         RETURN n,"is not a prime number"
+##      ELSE
+##         RETURN checkPrimeNumber(x,y-10
+##    RETURN  checkPrimeNumber(n,round(SquareRoot(n))
+##
+## UserInput<- INPUT "Enter a number to check whether it is a prime number: "
+## Print pn(userInput)
 def pn(n):
     def checkPrimeNumber(x,y):
         if (y<=1):
