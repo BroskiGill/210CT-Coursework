4ee0e8a3315645c99501d7a5fff15c962ef43490
BroskiGill <onkar@hotmail.co.uk>
2016-12-02 17:31:57 +0000

    Update Q7Coursework.py

--- Q7Coursework.py
+++ Q7Coursework.py
@@ -14,12 +14,16 @@ import math
 ## Print pn(userInput)
 def pn(n):
     def checkPrimeNumber(x,y):
+        # calculates whethers its less than 1
         if (y<=1):
             return (str(n)+" is a prime number")
+        # calculates whether numbers has factors
         elif (x % y==0):
             return (str(n)+" is not a prime number")
         else:
+            # returns both calculation whether its prime or not
             return checkPrimeNumber(x,y-1)
+         # rounds the numbers it does shows for example 4.7758488484848
     return checkPrimeNumber(n,round(math.sqrt(n)))
 
 userInput=int(input("Enter a number to check whether it is a prime number: "))
